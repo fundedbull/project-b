68e8541c700349f195a7b1077af7b878a4426b0d
Fabs <15038234+lumix103@users.noreply.github.com>
2025-03-08 15:54:17 -0800

    Fixed Searach params error with a suspense bounds

--- src/components/ui/SubNavLinks.tsx
+++ src/components/ui/SubNavLinks.tsx
@@ -2,28 +2,30 @@
 
 import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
-import { FC } from "react";
+import { FC, Suspense } from "react";
 
 const SubNavLink: FC<{ K: string; V: string; D: string }> = ({ K, V, D }) => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   return (
-    <Link
-      href={{
-        pathname,
-        query: {
-          ...Object.fromEntries(searchParams.entries()),
-          [K]: encodeURIComponent(V),
-        },
-      }}
-      className={`${
-        decodeURIComponent(searchParams.get(K) || D) === V
-          ? "text-blue"
-          : "text-white"
-      } uppercase`}
-    >
-      {V}
-    </Link>
+    <Suspense>
+      <Link
+        href={{
+          pathname,
+          query: {
+            ...Object.fromEntries(searchParams.entries()),
+            [K]: encodeURIComponent(V),
+          },
+        }}
+        className={`${
+          decodeURIComponent(searchParams.get(K) || D) === V
+            ? "text-blue"
+            : "text-white"
+        } uppercase`}
+      >
+        {V}
+      </Link>
+    </Suspense>
   );
 };
 
